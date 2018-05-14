772672d861353ea258c0b115425f33f84681fae0
Wesley Adiel <wesleyadiel@hotmail.com>
2018-05-14 14:33:13 -0300

    Primeiro commit com os arquivos do projeto

---
+++ cmake-build-debug/CMakeFiles/APS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/APS.dir/main.c.obj"
+  "APS.pdb"
+  "APS.exe"
+  "APS.exe.manifest"
+  "libAPS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/APS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
